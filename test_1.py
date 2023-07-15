7a8c675e601b0337e814c90a0f1d8f21976f66bc
amoshochman <amoshochman1982@gmail.com>
2023-07-15 11:05:58 +0300

    changing test to see if github action fails

--- test_1.py
+++ test_1.py
@@ -2,6 +2,7 @@ from main import app
 
 
 def test_hello_world():
+    assert False
     response = app.test_client().get("/")
     assert response.status_code == 200
     assert response.data == b"Hello, World!"
